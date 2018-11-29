c086b6a441850a5ec53a5c4bed20afa8afb4dc8d
ahmaddregat <ahmad.dregat.97@hotmail.com>
2018-11-29 23:54:23 +0200

    adding the Variables with constructor in this method

--- OOP_EX2-Ex4/src/GIS/myGIS_element.java
+++ OOP_EX2-Ex4/src/GIS/myGIS_element.java
@@ -4,23 +4,52 @@ import Geom.Geom_element;
 import Geom.Point3D;
 
 public class myGIS_element implements GIS_element {
-private met
+	
+	private MetaData MD ;
+	private Point3D gps;
+	
+	
+	public myGIS_element() {
+		// TODO Auto-generated constructor stub
+		MD=new MetaData();
+		setGps(new Point3D(0,0,0));
+	}
+	public myGIS_element(String name, String date, String mac, String authMode,
+			double lon, double lat, double alt) {
+		MD=new MetaData(name ,date,mac,authMode);
+		this.gps=new Point3D(lat,lon,alt);
+	}
+	public myGIS_element(String name, String date, String mac, String authMode,Point3D cd) {
+		this.MD=new MetaData(name, date, mac, authMode);
+		this.gps=new Point3D(cd);
+	}
+	public myGIS_element(MetaData MD ,double lon , double lat,double alt) {
+	this.MD=new MetaData(MD);
+	this.gps=new Point3D(lat,lon,alt);
+	}
+
 	@Override
 	public Geom_element getGeom() {
 		// TODO Auto-generated method stub
-		return null;
+		return this.gps;
 	}
 
 	@Override
 	public Meta_data getData() {
 		// TODO Auto-generated method stub
-		return null;
+		return this.MD;
 	}
 
 	@Override
 	public void translate(Point3D vec) {
 		// TODO Auto-generated method stub
-		
+
+	}
+	public Point3D getGps() {
+		return gps;
+	}
+	public void setGps(Point3D gps) {
+		this.gps = gps;
 	}
 
 }
