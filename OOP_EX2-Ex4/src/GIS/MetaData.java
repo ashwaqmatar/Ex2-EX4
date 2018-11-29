d75e48b604e4e8dfea26ff16358cccf0776b4592
ahmaddregat <ahmad.dregat.97@hotmail.com>
2018-11-29 23:37:18 +0200

    complete the metadata implements

--- OOP_EX2-Ex4/src/GIS/MetaData.java
+++ OOP_EX2-Ex4/src/GIS/MetaData.java
@@ -3,24 +3,35 @@ package GIS;
 import Geom.Point3D;
 
 public class MetaData implements Meta_data {
-	public metaData() {
-		
+
+	private long data;
+	Point3D Orientation;
+
+
+	public MetaData(long data,Point3D orientation) {
+		this.data = data;
+		this.Orientation=orientation;
+
 	}
-Long UTC ;
-point3D Orientation;
+	/**
+	 * returns the Universal Time Clock associated with this data;
+	 */
 	@Override
 	public long getUTC() {
 		// TODO Auto-generated method stub
-		return UTC;
+		return this.data;
 	}
+	
 
 	@Override
 	public Point3D get_Orientation() {
 		// TODO Auto-generated method stub
-		return Orientation;
+		return null ;
 	}
+	@Override
 	public String toString() {
-		
+		 return "" + getUTC();
+
 	}
 
 }
