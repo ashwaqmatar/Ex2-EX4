f5c389c96735066e434c834705c16099c1c544a5
ahmaddregat <ahmad.dregat.97@hotmail.com>
2018-11-30 00:35:47 +0200

    adding and fixing the GISlayer and GIS_project

--- OOP_EX2-Ex4/src/GIS/GISlayer.java
+++ OOP_EX2-Ex4/src/GIS/GISlayer.java
@@ -6,91 +6,96 @@ import java.util.Iterator;
 import java.util.Set;
 
 public class GISlayer implements GIS_layer {
+	private MetaData data;
+	public GISlayer(MetaData data) {
+	super();
+	this.data=data;
+	}
 Set<GIS_element> ST=new HashSet <GIS_element>(); 
 	// TODO Auto-generated constructor stub
 
 	@Override
 	public boolean add(GIS_element arg0) {
 		// TODO Auto-generated method stub
-		return false;
+		return ST.add(arg0);
 	}
 
 	@Override
 	public boolean addAll(Collection<? extends GIS_element> arg0) {
 		// TODO Auto-generated method stub
-		return false;
+		return ST.addAll(arg0);
 	}
 
 	@Override
 	public void clear() {
 		// TODO Auto-generated method stub
-		
+		ST.clear();
 	}
 
 	@Override
 	public boolean contains(Object arg0) {
 		// TODO Auto-generated method stub
-		return false;
+		return ST.contains(arg0);
 	}
 
 	@Override
 	public boolean containsAll(Collection<?> arg0) {
 		// TODO Auto-generated method stub
-		return false;
+		return ST.containsAll(arg0);
 	}
 
 	@Override
 	public boolean isEmpty() {
 		// TODO Auto-generated method stub
-		return false;
+		return ST.isEmpty();
 	}
 
 	@Override
 	public Iterator<GIS_element> iterator() {
 		// TODO Auto-generated method stub
-		return null;
+		return ST.iterator();
 	}
 
 	@Override
 	public boolean remove(Object arg0) {
 		// TODO Auto-generated method stub
-		return false;
+		return ST.remove(arg0);
 	}
 
 	@Override
 	public boolean removeAll(Collection<?> arg0) {
 		// TODO Auto-generated method stub
-		return false;
+		return ST.removeAll(arg0);
 	}
 
 	@Override
 	public boolean retainAll(Collection<?> arg0) {
 		// TODO Auto-generated method stub
-		return false;
+		return ST.retainAll(arg0);
 	}
 
 	@Override
 	public int size() {
 		// TODO Auto-generated method stub
-		return 0;
+		return ST.size();
 	}
 
 	@Override
 	public Object[] toArray() {
 		// TODO Auto-generated method stub
-		return null;
+		return ST.toArray();
 	}
 
 	@Override
 	public <T> T[] toArray(T[] arg0) {
 		// TODO Auto-generated method stub
-		return null;
+		return ST.toArray(arg0);
 	}
 
 	@Override
 	public Meta_data get_Meta_data() {
 		// TODO Auto-generated method stub
-		return null;
+		return data;
 	}
 
 }
