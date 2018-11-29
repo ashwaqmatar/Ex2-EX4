616c45e003d3af1426a95f409a1d241effe0a2aa
ahmaddregat <ahmad.dregat.97@hotmail.com>
2018-11-29 15:33:42 +0200

    starting in the package gis and goem and we will fixing

---
+++ OOP_EX2-Ex4/src/GIS/GISlayer.java
@@ -0,0 +1,96 @@
+package GIS;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class GISlayer implements GIS_layer {
+Set<GIS_element> ST=new HashSet <GIS_element>(); 
+	// TODO Auto-generated constructor stub
+
+	@Override
+	public boolean add(GIS_element arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean addAll(Collection<? extends GIS_element> arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean contains(Object arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean containsAll(Collection<?> arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Iterator<GIS_element> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean remove(Object arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean removeAll(Collection<?> arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean retainAll(Collection<?> arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object[] toArray() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <T> T[] toArray(T[] arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Meta_data get_Meta_data() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ OOP_EX2-Ex4/src/GIS/MetaData.java
@@ -0,0 +1,26 @@
+package GIS;
+
+import Geom.Point3D;
+
+public class MetaData implements Meta_data {
+	public metaData() {
+		
+	}
+Long UTC ;
+point3D Orientation;
+	@Override
+	public long getUTC() {
+		// TODO Auto-generated method stub
+		return UTC;
+	}
+
+	@Override
+	public Point3D get_Orientation() {
+		// TODO Auto-generated method stub
+		return Orientation;
+	}
+	public String toString() {
+		
+	}
+
+}
