62040d73f28ad5e3c2c0455ec3554cbba54caa54
ahmaddregat <ahmad.dregat.97@hotmail.com>
2018-12-02 22:13:28 +0200

    test
    Signed-off-by: ahmaddregat <ahmad.dregat.97@hotmail.com>

--- OOP_EX2-Ex4/src/Coords/coords_converter.java
+++ OOP_EX2-Ex4/src/Coords/coords_converter.java
@@ -3,11 +3,11 @@ package Coords;
 import Geom.Point3D;
 
 /**
- * This interface represents a basic coordinate system converter, including:
- * 1. The 3D vector between two lat,lon, alt points 
- * 2. Adding a 3D vector in meters to a global point.
- * 3. convert a 3D vector from meters to polar coordinates
- * @author Boaz Ben-Moshe
+ *   This interface represents a basic coordinate system converter, including:
+ *   1. The 3D vector between two lat,lon, alt points 
+ *   2. Adding a 3D vector in meters to a global point.
+ *   3. convert a 3D vector from meters to polar coordinates
+ *   @author Boaz Ben-Moshe
  *
  */
 public interface coords_converter {
