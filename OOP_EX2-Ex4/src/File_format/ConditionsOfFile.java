41b2ae85f9f5d53122ef40e09889a361c8c6e0e2
ashwaq matar <33615853+ashwaqmatar@users.noreply.github.com>
2018-12-02 16:02:36 +0200

    Add files via upload

---
+++ OOP_EX2-Ex4/src/File_format/ConditionsOfFile.java
@@ -0,0 +1,8 @@
+package File_format;
+import java.io.File;
+
+public interface ConditionsOfFile {
+	
+	boolean filterInfo(String data);
+
+}
---
+++ OOP_EX2-Ex4/src/File_format/FileBuilder.java
@@ -0,0 +1,16 @@
+package File_format;
+/**
+ * 
+ */
+
+/**
+ * @author Ali Masarweh
+ *
+ */
+public interface FileBuilder {
+	
+	String Create();
+	String Exit();
+	String addInfo(Object Data);
+
+}
